f719b19e02897eaf41054c6d08ead98630843e94
3 <79239887+Whsiddharth@users.noreply.github.com>
2022-05-28 13:47:29 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function take_snapshot(){
+    save('myFilterImage.png');
+}
+function preload(){
+
+}
+function draw(){
+
+}
+function setup(){
+    canvas=createCanvas(300,250);
+    canvas.center();
+}
